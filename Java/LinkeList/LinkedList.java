4be213870c06f30f19c9acb9a594d41f8c73deb6
Gab Amba <gamba@gabbydgab.com>
2014-08-01 17:11:05 +0800

    Initial LinkedList by Group A

---
+++ Java/LinkeList/LinkedList.java
@@ -0,0 +1,159 @@
+public class LinkedList
+{
+    protected Element head;
+    // protected Element tail;
+  
+    public LinkedList()
+    {
+        purge();
+    }
+  
+    public void purge()
+    {
+        //implementation
+        head = null;
+    }
+  
+    public Element getHead()
+    {
+        return head;
+    }
+  
+    public Element getTail()
+    {
+        Element elementTracker = head;
+     
+        while(elementTracker.getNext() != null)
+        {
+            elementTracker = elementTracker.getNext();
+        }
+    
+        return elementTracker;
+    }
+  
+    public boolean isEmpty()
+    {
+        return (head == null);
+    }
+  
+    public Object getFirst() throws Exception
+    {
+        if(isEmpty())
+            throw new ContainerEmptyException();
+            
+        return getHead().getDatum();
+    }
+  
+    public Object getLast() throws Exception
+    {
+        if(isEmpty())
+          throw new ContainerEmptyException();
+          
+        return getTail().getDatum();
+    }
+  
+    public void prepend(Object obj)
+    {     
+        head = new Element(obj, head);
+    }
+  
+    public void append(Object obj)
+    {
+        if(getHead() == null)
+            head = new Element(obj, null);
+        else
+            getTail().setNext(new Element(obj,null));
+    }
+  
+    public void assign(LinkedList list)
+    {
+        if(list != this)
+        {
+            purge();
+        
+            for(Element ptr = list.head; ptr != null; ptr = ptr.next)
+                append(ptr.datum);
+        }
+    }
+  
+    public void extract(Object item) throws Exception
+    {
+        Element ptr=head;
+        Element prevPtr=null;
+        
+        while (ptr!=null&&ptr.datum!=item)
+        {
+            prevPtr=ptr;
+            ptr=ptr.next;
+        }
+        
+        if (ptr==null)
+            throw new IllegalArgumentException();
+            
+        if (ptr==head)
+            head=ptr.next;
+        else
+            prevPtr.next=ptr.next;
+            
+        if(ptr==getTail())
+            getTail().setNext(prevPtr);
+    }
+  
+    public final class Element
+    {
+        protected Object datum;
+        protected Element next;
+    
+        Element(Object datum, Element next)
+        {
+            this.datum = datum;
+            this.next = next;
+        }
+    
+    public Object getDatum()
+    {
+      return datum;
+    }
+    
+    public Element getNext()
+    {
+      return next;
+    }
+    
+    public void setNext(Element next)
+    {
+      this.next = next;
+    }
+    
+    public void setDatum(Object datum)
+    {
+      this.datum = datum;
+    }
+    
+    public void insertAfter(Object item)
+    {      
+      next=new Element (item, next);
+      if (getTail()==this)
+        getTail().setNext(next);     
+    }
+    
+    public void insertBefore(Object item)
+    {
+      Element tmp=new Element (item,this);
+      if (this==getHead())
+        head=tmp;
+      else
+      {
+        Element prevPtr= getHead();
+        
+        while (prevPtr!=null&&prevPtr.next!=this)
+          prevPtr=prevPtr.next;
+        prevPtr.next=tmp;
+      }
+    }
+  }
+  
+  
+} // class LinkedList
+
+
