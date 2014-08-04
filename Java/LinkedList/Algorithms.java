7ced5bde6f5c09dc46e6a24ee953780b3654dac1
gabbydgab <gamba@gabbydgab.com>
2014-08-04 12:45:26 +0800

    added calculator implementation

--- Java/LinkedList/Algorithms.java
+++ Java/LinkedList/Algorithms.java
@@ -1,19 +1,58 @@
-
 public class Algorithms
 {
+    public static void main(String [] args)
+    {
+        calculator(new java.io.InputStreamReader(System.in), System.out);
+    }
   
-  /**
-   * copy 6.1.3.2
-   * 
-  public static void calculator(Reader in, PrintWriter out) throws IOException
-  {}
-  
-  */
-  
-  public static void main(String [] args)
-  {
-    calculator(new java.io.InputStreamReader(System.in), System.out);
-  }
-  
-  
+    public static void calculator(Reader in, PrintWriter out)
+      throws IOException
+    {
+      Stack stack = new StackAsLinkedList();
+      
+      int i;
+      while((i = in.read) > 0)
+      {
+        char c = (char) i;
+        
+        if(Character.isDigit(c))
+        {
+          stack.push(new Int(Character.digit(c, 10)));
+        }
+        else if(c == '+')
+        {
+          Int arg2 = (Int) stack.pop();
+          Int arg1 = (Int) stack.pop();
+          stack.push(new Int(arg1.intValue() + arg2.intValue()));
+        }
+        else if(c == '*')
+        {
+          Int arg2 = (Int) stack.pop();
+          Int arg1 = (Int) stack.pop();
+          stack.push(new Int(arg1.intValue() * arg2.intValue()));
+        }
+        else if(c == '-')
+        {
+          Int arg2 = (Int) stack.pop();
+          Int arg1 = (Int) stack.pop();
+          stack.push(new Int(arg1.intValue() - arg2.intValue()));
+        }
+        else if(c == '/')
+        {
+          Int arg2 = (Int) stack.pop();
+          Int arg1 = (Int) stack.pop();
+          
+          if(arg2 == 0)
+            throw new Exception("Divisor cannot be zero");
+          
+          stack.push(new Int(arg1.intValue() / arg2.intValue()));
+        }
+        else if(c == '=')
+        {
+          Int arg = (Int) stack.pop();
+          out.println(arg);
+        }
+      }
+      
+    }  
 }
