718804cf8fff38f79d1c22c87fef374320e2d04b
gabbydgab <gamba@gabbydgab.com>
2014-08-04 10:28:45 +0800

    added initial implementation

---
+++ Java/LinkedList/algorithms.java
@@ -0,0 +1,18 @@
+
+public class Algorithms
+{
+  
+  /**
+   * copy 6.1.3.2
+   * 
+  public static void calculator(Reader in, PrintWriter out) throws IOException
+  {}
+  
+  */
+  
+  public static void main(String [] args)
+  {
+    calculator(new java.io.InputStreamReader(System.in), System.out);
+  }
+  
+}
