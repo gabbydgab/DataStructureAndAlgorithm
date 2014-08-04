23b47a35d14453925805bc82573aee3f2e306b87
gabbydgab <gamba@gabbydgab.com>
2014-08-04 11:40:37 +0800

    added methods push, pop, getTop

--- Java/LinkedList/StackAsLinkedList.java
+++ Java/LinkedList/StackAsLinkedList.java
@@ -1,4 +1,42 @@
-public class StackAsLinkedList
+public class StackAsLinkedList 
+    extends AbstractContainer
+    implements Stack
 {
-
+    protected LinkedList list;
+    
+    public StackAsLinkedList()
+    {
+        list = new LinkedList();
+    }
+    
+    public void purge()
+    {
+        list.purge();
+        count = 0;
+    }
+   
+    public void push (Object object)
+    {
+        list.prepend(object);
+        ++count;        
+    }
+    
+    public Object pop()
+    {
+        if(count==0)
+            throw new ContainerEmptyException();
+        
+        Object result = list.getFirst();
+        list.extract(result);
+        --count;
+        return result;
+    }
+    
+    public Object getTop()
+    {
+        if(count==0)
+            throw new ContainerEmptyException();
+        
+        return list.getFirst();
+    }    
 }
